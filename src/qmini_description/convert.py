38165d4e8d15634703ba7d492c3da34d62aee1e8
SuJerry01 <sujerry0525@gmail.com>
2025-08-19 18:07:38 +0800

    fix

--- src/qmini_description/convert.py
+++
@@ -1,6 +0,0 @@
-import mujoco
-# Load URDF (MuJoCo treats .urdf as XML input)
-model = mujoco.MjModel.from_xml_path("Qmini.urdf")
-# Export MJCF to XML
-mujoco.mj_saveLastXML("qmini.xml", model)
-EOF
